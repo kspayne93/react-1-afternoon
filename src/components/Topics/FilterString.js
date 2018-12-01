e88ee1fdbc1bf72a5eba8df4c94af83e8f6e2380
kspayne93 <kyle@investavante.com>
2018-11-30 17:05:56 -0700

    Created Topic Components & imported them into Topic Browser

---
+++ src/components/Topics/FilterString.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FilterString extends Component {
+   render () {
+      return (
+         <p> Filter Object Component </p>
+      )
+   }
+}
+
+export default FilterString
